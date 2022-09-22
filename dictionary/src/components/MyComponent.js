09d02b6baa12819964ecf09cd93220c7d6ece629
Rafael Felipe <rfaeldel@outlook.com>
2022-09-22 09:03:55 -0300

    Update

--- dictionary/src/components/MyComponent.js
+++
@@ -1,9 +0,0 @@
-const MyComponent = () =>{
-    return(
-        <div>
-            <h2>Eu estou sendo reaproveitado em varios lugares !</h2>
-        </div>
-    )
-}
-
-export default MyComponent;
